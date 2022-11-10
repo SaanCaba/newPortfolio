e1a44b2808cd9b4c0ee4608291e43ed3b69aae9c
Santiago Caballero <saantyc12.caballero@gmail.com>
2022-11-10 18:33:18 -0300

    change-wpp

--- src/components/WhatsAppIcon.jsx
+++ src/components/WhatsAppIcon.jsx
@@ -5,7 +5,7 @@ import './styles/IconWpp.css'
 function WhatsAppIcon() {
   return (
     <div className='cont-iconwpp'>
-            <ReactWhatsapp className='link-wpp' number="+54 1160198446" message='Hola'>
+            <ReactWhatsapp className='link-wpp' number="+54 1160198446" message='Hola, quieres enviarle un mensaje a Santiago?'>
             {/* <WhatsappIcon className='wpp-icon'  /> */}
             <BsWhatsapp size={32} color="#35f067" />
             </ReactWhatsapp>
